3088552c18e408750f624afa3ebf0cd5de3dce89
David Galstyan <david.galstyan.1905@gmail.com>
2023-05-10 16:14:15 +0400

    Game Creation Page UI

---
+++ app/src/main/java/com/example/programmingarea/CreateGameActivity.java
@@ -0,0 +1,15 @@
+package com.example.programmingarea;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateGameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_submit);
+
+
+    }
+}
